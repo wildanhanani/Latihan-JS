4a985a94f95b5f09637cb1db2ea0215156bc436c
Wildan Hanani <wildanhanani@gmail.com>
2020-03-04 23:46:47 +0700

    latihan 2

--- biodata.js
+++ biodata.js
@@ -2,6 +2,7 @@ function biodata() {
 	var biodataLengkap = {
 		nama : "Muhammad Wildan Hanani",
 		alamat : "Makamhaji Kartasura",
+		umur : console.log(umur(2019,1996)),
 		hobi : ["futsal", "renang", "dll"],
 		school : {highschool:"Sma Batik 1 Ska", university:"Universitas Muhammadiyah Surakarta"},
 		skills : [{php:"70"},{js:"80"}]
@@ -11,3 +12,20 @@ function biodata() {
 }
 
 console.log(biodata());
+
+function umur(thnskrng, tahunlhir){
+	hasil = thnskrng-tahunlhir;
+	return hasil;
+}
+
+function tambah(a, b){
+	hasilTambah = a + b;
+	return hasilTambah;
+}
+
+console.log(tambah(5,2));
+// var nilai1 = 50;
+// var nilai2 = 25;
+// var hasilpenambahan = tambah(nilai1, nilai2);
+
+// console.log(hasilpenambahan);
